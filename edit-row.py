205412cba94941a6c7eabf04725d6d1766241df4
kenannn <kenanjoshua3454@gmail.com>
2021-04-01 00:39:28 +0800

    Edit or Update

--- edit-row.py
+++ edit-row.py
@@ -7,10 +7,16 @@ from strap import connection
 
 form = cgi.FieldStorage()
 ID = form.getvalue("passID")
-print(form.getvalue("passID"))
 
 cursor = connection.cursor()
 cursor.execute("select * from user where id = %s;" % ID)
 result = cursor.fetchall()
+print('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">')
+print('<p>Name: %s</p>' % result[0][1])
+print('<p>Email: %s</p>' % result[0][3])
+print('<p>Password:  ')
 
-print(result)    
+for i in range(len(result[0][2])):
+    print('*')
+print('</p>')
+
